5e131e1bd2638633f52ac3a41bceb8a51f13e6f2
amigin <amigin@gmail.com>
2022-08-08 14:08:18 +0300

    cargo update

--- src/socket_io_list.rs
+++ src/socket_io_list.rs
@@ -5,7 +5,7 @@ use crate::MySocketIoConnection;
 #[async_trait::async_trait]
 pub trait SocketIoList<TCustomData: Sync + Send + 'static> {
     async fn add(&self, socket_io_connection: Arc<MySocketIoConnection<TCustomData>>);
-    async fn remove(&self, socket_io_id: &str);
+    async fn remove(&self, socket_io_id: &str) -> Option<Arc<MySocketIoConnection<TCustomData>>>;
     async fn get(&self, socket_io_id: &str) -> Option<Arc<MySocketIoConnection<TCustomData>>>;
     async fn get_by_web_socket_id(
         &self,
